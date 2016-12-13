fac5aaabcf69319d30d5488548e1d667bb2104c7
Charles Oliver Nutter <headius@headius.com>
2016-12-13 05:56:31 -0600

    First version.

---
+++ src/main/java/com/headius/unsafe/UnsafeFences.java
@@ -0,0 +1,50 @@
+package com.headius.unsafe;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class UnsafeFences {
+    public static final boolean SUPPORTS_FENCES = supportsFences();
+
+    public static void fullFence() {
+        U.fullFence();
+    }
+
+    public static void loadFence() {
+        U.loadFence();
+    }
+
+    public static void storeFence() {
+        U.storeFence();
+    }
+
+    private UnsafeFences(){}
+
+    private static final sun.misc.Unsafe U = loadUnsafe();
+
+    private static sun.misc.Unsafe loadUnsafe() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field f = unsafeClass.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            return (sun.misc.Unsafe) f.get(null);
+        } catch (Exception e) {
+            return null;
+        } catch (NoClassDefFoundError ncdfe) {
+            // Google AppEngine raises NCDFE for Unsafe rather than CNFE
+            return null;
+        }
+    }
+
+    private static boolean supportsFences() {
+        if(U == null)
+            return false;
+        try {
+            Method m = U.getClass().getDeclaredMethod("fullFence");
+            if(m != null)
+                return true;
+        } catch (Exception e) {
+        }
+        return false;
+    }
+}
